1a0d57b5cdd2b8aafd8060cfa0a920f94f6aec76
Akshat Pandya <akshatpandya27072004@gmail.com>
2024-12-29 18:06:39 +0530

    added new endpoint

--- todo_backend/urls.py
+++ todo_backend/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from .views import home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', home_view, name='home'),  # Root endpoint
     path('api/', include('taskflow.urls')),  # Include taskflow app URLs
 ]
